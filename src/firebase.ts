a386996ad07f8f324ede2de3a18445783ce9bfe1
braulpablo-maker <braulpablo@gmail.com>
2026-07-11 17:56:35 -0300

    fix: authenticate Firebase clients anonymously

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,5 +1,6 @@
 import { initializeApp } from "firebase/app";
 import { getDatabase } from "firebase/database";
+import { getAuth, signInAnonymously } from "firebase/auth";
 
 const firebaseConfig = {
   apiKey: "AIzaSyAzMUxOnwIpatNglROieQVg8cuhMX9taxk",
@@ -13,4 +14,6 @@ const firebaseConfig = {
 };
 
 export const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+export const authReady = signInAnonymously(auth);
 export const database = getDatabase(app);
